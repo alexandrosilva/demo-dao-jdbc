e491b59363bdf78b4f85d56cae9bd507ae44bf94
Alexandro Silva <alexandrorsilva2@gmail.com>
2020-03-30 22:08:54 -0300

    insert implemetation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -21,12 +22,20 @@ public class Program {
 		System.out.println("\n=== TEST 2: seller findByDepartament ===");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 		
 		System.out.println("\n=== TEST 3: seller findAll ===");
 		List<Seller> listAll = sellerDao.findAll();
 		
 		for (Seller obj : listAll) {
 			System.out.println(obj);
-		}		
+		}
+		
+		System.out.println("\n=== TEST 4: seller insert ===");
+		Seller newseller = new Seller(null, "Greg", "greg@gmail.com" , new Date(), 4000.0, department);
+		sellerDao.insert(newseller);
+		System.out.println("Inserted! New id = " + newseller.getId());
 	}
 }
